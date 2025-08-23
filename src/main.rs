7aead1eab36e14b13baa48566276753dceaa7ad2
Inspiaaa <inspiaaa@gmail.com>
2025-08-23 14:51:32 +0200

    Add more config options
    - Add config options for line width and line color

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
 use clap::Parser;
+use crate::convert::LineStyleConfig;
 
 mod convert;
 
@@ -19,6 +20,15 @@ struct Cli {
     /// Quite: Disable logging
     #[arg(short = 'q', long = "quiet")]
     quiet: bool,
+
+    /// Line color.
+    /// Remember to include the alpha value at the end, e.g. "#ff0000ff" for red.
+    #[arg(short = 'c', long = "color", default_value = "#FF4136FF")]
+    line_color: String,
+
+    /// Line width
+    #[arg(short = 'w', long = "width", default_value = "1.0")]
+    line_width: f64
 }
 
 fn main() {
@@ -41,8 +51,13 @@ fn main() {
     let output_file = File::create(output_path.as_path()).expect("Unable to create output file.");
 
     if !args.quiet { println!("Converting..."); }
+    
+    let line_style = LineStyleConfig {
+        color: args.line_color,
+        width: args.line_width,
+    };
 
-    convert::convert(input_file_contents.as_slice(), output_file).unwrap();
+    convert::convert(input_file_contents.as_slice(), output_file, &line_style).unwrap();
 
     if !args.quiet {
         println!("Finished conversion. Wrote output to '{}'.", output_path.display())
