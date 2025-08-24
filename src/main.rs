4bf4a25007d549c569dfff9f4053f3b7eb411edd
Inspiaaa <inspiaaa@gmail.com>
2025-08-25 00:54:00 +0200

    Create unified CLI with subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+use gpx_tools::{gpx_clean, gpx_reduce_points, gpx_to_kml};
+
+#[derive(Subcommand)]
+enum Command {
+    Clean(gpx_clean::cli::Args),
+    ReducePoints(gpx_reduce_points::cli::Args),
+    GpxToKml(gpx_to_kml::cli::Args),
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.command {
+        Command::Clean(args) => gpx_clean::cli::run_cli_with_args(args),
+        Command::ReducePoints(args) => gpx_reduce_points::cli::run_cli_with_args(args),
+        Command::GpxToKml(args) => gpx_to_kml::cli::run_cli_with_args(args),
+    }
 }
