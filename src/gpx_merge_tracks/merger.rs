5df0840d304249d8929cc8773e0fcb7b466ec353
Inspiaaa <inspiaaa@gmail.com>
2025-08-26 14:12:35 +0200

    Fix track and segment ordering in merge_tracks

--- src/gpx_merge_tracks/merger.rs
+++ src/gpx_merge_tracks/merger.rs
@@ -5,8 +5,10 @@ pub fn merge_tracks(gpx: &mut Gpx, merged_name: String) {
         name: Some(merged_name),
         ..Default::default()
     };
-
+    
+    gpx.tracks.reverse();
     while let Some(mut track) = gpx.tracks.pop() {
+        track.segments.reverse();
         while let Some(segment) = track.segments.pop() {
             merged_track.segments.push(segment);
         }
